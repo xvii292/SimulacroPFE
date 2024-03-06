d840c9ef7d988b1921522dbefde8eba1baeba173
xvii292 <130442788+xvii292@users.noreply.github.com>
2024-03-06 12:12:47 +0100

    Nuevo footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import PiedePagina from './components/footer';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <main></main>
+
+      <PiedePagina/>
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import logoEmp from "../assets/logov1.png";
+import logoInsta from "../assets/instagram.png";
+import logoFace from "../assets/facebook.png";
+import logoX from "../assets/x.png";
+import './footer.css';
+
+const Pie = () => {
+  return (
+    <footer>
+      <div class="footer-container">
+        <div class="footer-section">
+          <h4>Hydroponic MnZ</h4>
+          <img src={logoEmp} alt="Hydroponic MnZ logo" class="footer-logo"/>
+        </div>
+        <div class="footer-section">
+          <h4>Servicios</h4>
+          <ul>
+            <li>Consultoría Comercial de Venta</li>
+            <li>Consultoría de cultivo</li>
+            <li>Aviso legal</li>
+            <li>Política de Privacidad</li>
+            <li>Política de Cookies</li>
+          </ul>
+        </div>
+        <div class="footer-section">
+          <h4>¡Encuéntranos!</h4>
+          <ul>
+            <li>Teléfono: +34 926263456</li>
+            <li>Email: hydroPonicsMnZ@gmail.com</li>
+            <li>Dirección: Calle 45, Manzanares (13200) Ciudad Real</li>
+          </ul>
+        </div>
+        <div class="footer-section">
+          <h4>Redes Sociales</h4>
+          <div class="social-media-links">
+            <a href="your_facebook_profile" target="_blank"><img src={logoFace} alt="Facebook"/></a>
+            <a href="your_twitter_profile" target="_blank"><img src={logoX} alt="X"/></a>
+            <a href="your_instagram_profile" target="_blank"><img src={logoInsta} alt="Instagram"/></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Pie;
