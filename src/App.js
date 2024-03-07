23f890f0543d4f01eb6f5f081475160868342f37
xvii292 <130442788+xvii292@users.noreply.github.com>
2024-03-07 09:45:42 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import PiedePagina from './components/footer';
 import Productos from './components/productos';
 import Instalaciones from './components/instalacion';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Navbar/>
       </header>
       <main>
 
